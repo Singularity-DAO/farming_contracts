ff1c963b909df139d09de48ef3fc055f06c9f6da
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2021-07-23 18:07:56 +0530

    Added artifacts

--- migrations/3_SDAOTokenStaking.js
+++ migrations/3_SDAOTokenStaking.js
@@ -2,16 +2,23 @@ let Contract = require("@truffle/contract");
 let SDAOTokenStaking = artifacts.require("./SDAOTokenStaking.sol");
 
 
-// Need to update the same once the SDAO Token artifacts are deployed as an npm package
-//let TokenAbi = require("singularitydao-token-contracts/abi/SingularityDAOToken.json");
-//let TokenNetworks = require("singularitydao-token-contracts/networks/SingularityDAOToken.json");
-//let TokenBytecode = require("singularitydao-token-contracts/bytecode/SingularityDAOToken.json");
-//let Token = Contract({contractName: "SDAO", abi: TokenAbi, networks: TokenNetworks, bytecode: TokenBytecode});
+let TokenAbi = require("../SDAOTokenArtifacts/abi/SDAOToken.json");
+let TokenNetworks = require("../SDAOTokenArtifacts/networks/SDAOToken.json");
+let TokenBytecode = require("../SDAOTokenArtifacts/bytecode/SDAOToken.json");
+let Token = Contract({contractName: "SingularityDAO", abi: TokenAbi, networks: TokenNetworks, bytecode: TokenBytecode});
 
+// Token Contract Constants
+const name = "SingularityDAO Token"
+const symbol = "SDAO"
 
 module.exports = function(deployer, network, accounts) {
 
+    //deployer.deploy(SDAOTokenStaking,"0x5e94577b949a56279637ff74dfcff2c28408f049");
 
-    deployer.deploy(SDAOTokenStaking,"0x5e94577b949a56279637ff74dfcff2c28408f049");
+    Token.setProvider(web3.currentProvider)
+    Token.defaults({from: accounts[0], gas: 4000000});
+
+    // AirDrop Contract deployment 
+    deployer.deploy(Token, accounts[0], {overwrite: false, gas: 4000000}).then((TokenInstance) => deployer.deploy(SDAOTokenStaking, TokenInstance.address));
 
 };
